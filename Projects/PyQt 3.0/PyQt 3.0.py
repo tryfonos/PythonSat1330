754f060985ba85c5949307b9649f252316c56e0e
alkihuri <alkihuri@gmail.com>
2022-06-18 13:48:51 +0300

    Update PyQt 3.0.py

--- Projects/PyQt 3.0/PyQt 3.0.py
+++ Projects/PyQt 3.0/PyQt 3.0.py
@@ -1,3 +1,5 @@
+#presentation https://docs.google.com/presentation/d/14PsvFT_9wBGSXjAtZ1D0K909I5fUud2U4GPuVMDoypQ/edit#slide=id.g132709bd007_0_318
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow, QWidget, QLabel, QLineEdit
 import sys
